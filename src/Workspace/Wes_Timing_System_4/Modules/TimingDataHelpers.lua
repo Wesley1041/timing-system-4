9e36ad7aca03fa18327488a37974cec976712c2d
jak77 <jak77@aber.ac.uk>
2023-12-08 23:47:16 +0000

    fix

--- src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
+++ src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
@@ -4,18 +4,18 @@ local helper = {}
 ---@param data table Raw timing data
 ---@return table list Data as list
 function helper:GetSortedData(data: table)
-	
+
 	local list = {}
-	
+
 	for _, userData in pairs(data) do
 		table.insert(list, userData)
 	end
 
 	-- Sort list
 	table.sort(list, ComparePlayerData)
-	
+
 	return list
-	
+
 end
 
 --- Compares two player data instances to determine which one is higher than the other
@@ -23,9 +23,11 @@ end
 ---@param player2 table Player data
 ---@return boolean result True if player 1 should be above player 2
 function ComparePlayerData(player1: table, player2: table): boolean
-
-	return player2.BestLap.LapTime == nil or
-		   player1.BestLap.LapTime ~= nil and player1.BestLap.LapTime < player2.BestLap.LapTime
+	if player1.BestLap.LapTime == player2.BestLap.LapTime then return true end
+	if player1.BestLap.LapTime == nil and player2.BestLap.LapTime ~= nil then return false end
+	if player1.BestLap.LapTime ~= nil and player2.BestLap.LapTime == nil then return true end
+	if player2.BestLap.LapTime >= player1.BestLap.LapTime then return true end
+	return false
 
 end
 

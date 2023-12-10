cd1ef55d1a6303f0bb293459bbc63c49a0dedc34
jamburger71 <47787329+jamburger71@users.noreply.github.com>
2023-12-10 21:59:27 +0000

    Bugfix/critical bug where table sorting crashes UI (#14)
    * tweaked name
    
    * tweaked name again
    
    * amended coding standards
    
    * fix
    
    * last fix didnt work, new counterfix that compares userID when both bestlaps are nil
    
    * tidying up the code a bit
    
    * comments on functions
    
    ---------
    
    Co-authored-by: jak77 <jak77@aber.ac.uk>

--- src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
+++ src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
@@ -4,28 +4,34 @@ local helper = {}
 ---@param data table Raw timing data
 ---@return table list Data as list
 function helper:GetSortedData(data: table)
-	
+
 	local list = {}
-	
+
 	for _, userData in pairs(data) do
 		table.insert(list, userData)
 	end
 
 	-- Sort list
 	table.sort(list, ComparePlayerData)
-	
+
 	return list
-	
+
 end
 
 --- Compares two player data instances to determine which one is higher than the other
 ---@param player1 table Player data
 ---@param player2 table Player data
 ---@return boolean result True if player 1 should be above player 2
+--- First check is if both laptimes are nil, in which case it compares UserIDs
+--- Second check is if the player1 laptime is nil which automatically ranks them lower
+--- Third check is if the player2 laptime is nil in which the player1 is automatically ranked higher
+--- Final check compares not-nil laptimes
 function ComparePlayerData(player1: table, player2: table): boolean
-
-	return player2.BestLap.LapTime == nil or
-		   player1.BestLap.LapTime ~= nil and player1.BestLap.LapTime < player2.BestLap.LapTime
+	if player1.BestLap.LapTime == nil and player2.BestLap.LapTime == nil then return player1.UserId > player2.UserId end
+	if player1.BestLap.LapTime == nil then return false end
+	if  player2.BestLap.LapTime == nil then return true end
+	if player2.BestLap.LapTime >= player1.BestLap.LapTime then return true end
+	return false
 
 end
 

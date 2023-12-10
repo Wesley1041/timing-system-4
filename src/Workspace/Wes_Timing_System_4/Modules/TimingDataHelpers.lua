31f9c0a149fa8c500ceb6d524d78577bb276f9f9
jak77 <jak77@aber.ac.uk>
2023-12-10 16:56:56 +0000

    comments on functions

--- src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
+++ src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
@@ -22,6 +22,10 @@ end
 ---@param player1 table Player data
 ---@param player2 table Player data
 ---@return boolean result True if player 1 should be above player 2
+--- First check is if both laptimes are nil, in which case it compares UserIDs
+--- Second check is if the player1 laptime is nil which automatically ranks them lower
+--- Third check is if the player2 laptime is nil in which the player1 is automatically ranked higher
+--- Final check compares not-nil laptimes
 function ComparePlayerData(player1: table, player2: table): boolean
 	if player1.BestLap.LapTime == nil and player2.BestLap.LapTime == nil then return player1.UserId > player2.UserId end
 	if player1.BestLap.LapTime == nil then return false end

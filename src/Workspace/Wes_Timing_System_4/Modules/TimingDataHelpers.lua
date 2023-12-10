11b8a3206d93e20a487c0503d676d6986b4d21d7
jak77 <jak77@aber.ac.uk>
2023-12-10 16:52:08 +0000

    tidying up the code a bit

--- src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
+++ src/Workspace/Wes_Timing_System_4/Modules/TimingDataHelpers.lua
@@ -23,11 +23,9 @@ end
 ---@param player2 table Player data
 ---@return boolean result True if player 1 should be above player 2
 function ComparePlayerData(player1: table, player2: table): boolean
-	if player1.BestLap.LapTime == nil and player2.BestLap.LapTime == nil then 
-		return player1.UserId > player2.UserId
-	end
-	if player1.BestLap.LapTime == nil and player2.BestLap.LapTime ~= nil then return false end
-	if player1.BestLap.LapTime ~= nil and player2.BestLap.LapTime == nil then return true end
+	if player1.BestLap.LapTime == nil and player2.BestLap.LapTime == nil then return player1.UserId > player2.UserId end
+	if player1.BestLap.LapTime == nil then return false end
+	if  player2.BestLap.LapTime == nil then return true end
 	if player2.BestLap.LapTime >= player1.BestLap.LapTime then return true end
 	return false
 

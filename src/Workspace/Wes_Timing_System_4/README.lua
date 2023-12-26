cb563fbd5471fa3f670f673b2c374990fccd803e
Wesley <wesley1041@live.nl>
2023-12-26 17:28:12 +0100

    Update version number (RELEASE 1.2)

--- src/Workspace/Wes_Timing_System_4/README.lua
+++ src/Workspace/Wes_Timing_System_4/README.lua
@@ -1,7 +1,7 @@
 --[[
 ----------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------
-Wesley1041's Timing System 4 v1.1-beta
+Wesley1041's Timing System 4 v1.2-beta
 
 This fourth timing system is designed to be compatible with places that use StreamingEnabled.
 It is the most feature-rich and modular timing system yet.
